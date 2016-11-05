094fea87f24649914e7669f43b4939d9496936cb
Emmanuel Vargas B <emmanuel.vargas@ucr.ac.cr>
2016-11-05 11:47:59 -0600

    agrego al sistema de reportes las salidas de las interfaces activas y los puertos habilitados

--- src/reporte.sh
+++ src/reporte.sh
@@ -5,3 +5,19 @@ REPORTE=reporte-`date +%Y%m%d`.log
 
 echo "Reporte generado por $USER:" > $REPORTE
 echo " " >> $REPORTE
+echo " " >> $REPORTE
+
+echo "#######################################" >> $REPORTE
+echo "# Interfaces disponibles en el servidor" >> $REPORTE
+echo "#######################################" >> $REPORTE
+echo " " >> $REPORTE
+
+ifconfig >> $REPORTE
+
+
+echo "####################################" >> $REPORTE
+echo "# Puertos habilitados en el servidor" >> $REPORTE
+echo "####################################" >> $REPORTE
+echo " " >> $REPORTE
+
+netstat -latun | grep LISTEN >> $REPORTE
