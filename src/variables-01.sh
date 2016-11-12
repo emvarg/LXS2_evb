764505a651db69adb1500dccf7c81d397df394f4
Emmanuel Vargas B <emmanuel.vargas@ucr.ac.cr>
2016-11-12 10:37:52 -0600

    inicio de ejemplos de redireccionamiento de datos

---
+++ src/variables-01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "Estas variables estan definidas en ~/.bashrc"
+
+
+echo "\$MI_VAR1 es : $MI_VAR1"
+echo "\$MI_VAR2 es : $MI_VAR2"
+echo "\$MI_VAR3 es : $MI_VAR3"
+echo "\$MI_VAR4 es : $MI_VAR4"
+
+
