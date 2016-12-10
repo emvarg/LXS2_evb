47abaead2f8d3f134e441535c33845bd00cf7c18
Emmanuel Vargas B <emmanuel.vargas@ucr.ac.cr>
2016-12-10 09:46:30 -0600

    Ejemplo de if anidados

--- src/resumen.sh
+++ src/resumen.sh
@@ -7,16 +7,30 @@
 
 PARAMETROS=3
 
+# Para diferencias uso !=
 if [[ $# != $PARAMETROS ]]
 then
 	echo "Error:
 	La forma de utilizar el script es la siguiente
-	./resumen.sh <param1> <param2> <param3>
+	$0 <param1> <param2> <param3>
 	"
 	exit 1
+else
+	echo "El número de parámetros fue correcto, y es $#"
+
+	# Para igualdades uso ==
+	if [[ $2 == casa ]]
+	then
+		echo "El parámetro 2 es casa!"
+	else
+		echo "El parámetro 2 NO es casa"
+	fi
 fi
 
 echo "Los parámetros fueron"
 echo $@
+
+
+
 
 exit 0
